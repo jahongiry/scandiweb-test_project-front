c8843d36c9b48c94ef914591c84a27f1ba1dbcb9
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2023-05-05 10:02:09 +0500

    Add style

--- src/ProductList/ProductList.js
+++ src/ProductList/ProductList.js
@@ -44,21 +44,6 @@ function ProductList() {
 
       <div className='productContainer'>
         <ul>
-          <li className='eachProductContainer'>
-            <input
-              className='delete-checkbox'
-              id='delete-checkbox'
-              type='checkbox'
-            />
-            <p className='skuOnList'>NameTest000</p>
-            <p className='skuOnList'>NameTest001</p>
-            <p className='skuOnList'>NameTest002</p>
-            {/* <p>{product.name}</p>
-            <p>Price: {product.price}</p>
-            {product.size && <p>Size: {product.size}</p>}
-            {product.weight && <p>Weight: {product.weight}</p>}
-            {product.dimension && <p>Dimension {product.dimension}</p>} */}
-          </li>
           {products.map((product) => (
             <li className='eachProductContainer' key={product.sku}>
               <input
